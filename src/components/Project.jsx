7a448e03c768a59d27051c74d1c3be7c2e9e8250
lithana <lbn.nathalie@gmail.com>
2023-11-03 17:18:42 +0100

    Add Showcase Section

---
+++ src/components/Project.jsx
@@ -0,0 +1,238 @@
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import styled, { keyframes } from "styled-components"
+
+import colors from "../style/colors"
+import queries from "../style/queries"
+
+const Show = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 10rem;
+  border-top: 3px solid ${colors.light};
+
+  @media (max-width: ${queries.smallProject}px) {
+    height: 12rem;
+  }
+`
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  @media (max-width: ${queries.smallProject}px) {
+    flex-direction: column;
+  }
+`
+
+const TextContainer = styled.div`
+  text-align: end;
+  width: 100%;
+  padding: 0 1rem;
+  color: ${colors.dark};
+
+  @media (max-width: ${queries.smallProject}px) {
+    padding: 0;
+  }
+`
+
+const ProjectTitle = styled.h4`
+  font-size: 3.5rem;
+  font-weight: 100;
+  letter-spacing: -2px;
+  line-height: 3.5rem;
+`
+
+const Tools = styled.div`
+  height: 100%;
+  width: 20%;
+  display: flex;
+  gap: 0.7rem;
+  flex-wrap: wrap;
+  text-align: center;
+
+  @media (max-width: ${queries.medium}px) {
+    width: 41%;
+  }
+
+  @media (max-width: ${queries.smallProject}px) {
+    margin-top: 1rem;
+    width: 100%;
+  }
+
+  span {
+    color: ${colors.light};
+    background: ${colors.secondary};
+    flex: 1;
+    padding: 0.3rem 0.6rem;
+    border-radius: 10px;
+    text-transform: uppercase;
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
+`
+
+const ContentHovered = styled.div`
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+`
+
+const Image = styled.img`
+  position: absolute;
+  top: -6vw;
+  object-fit: cover;
+  z-index: 2;
+  width: 35vw;
+  margin-left: 8vw;
+  transform: rotate(-4deg);
+  pointer-events: none;
+
+  @media (max-width: ${queries.smallProject}px) {
+    display: none;
+  }
+`
+
+const Scroll = keyframes`
+100% {transform: translateX(-50%)}
+`
+
+const TextHovered = styled.p`
+  width: fit-content;
+  font-size: 5rem;
+  opacity: 0.5;
+  color: ${colors.light};
+  font-weight: 100;
+  animation: ${Scroll} 10s linear infinite;
+
+  @media (max-width: ${queries.smallProject}px) {
+    font-size: 6rem;
+  }
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+  justify-content: center;
+  height: 100%;
+  position: absolute;
+  z-index: 3;
+  top: 0;
+  right: 0;
+  margin-right: 5vw;
+
+  @media (max-width: ${queries.smallProject}px) {
+    width: 100%;
+    margin-right: 0;
+    flex-wrap: wrap;
+    align-content: center;
+  }
+`
+
+const Button = styled.a`
+  border: none;
+  background: ${colors.secondary};
+  padding: 0.6rem 1.2rem;
+  border-radius: 50px;
+  text-align: center;
+  box-shadow: -6px 5px ${colors.light};
+  font-weight: 600;
+  white-space: nowrap;
+  cursor: pointer;
+  color: ${colors.light};
+  transition: 300ms;
+
+  &:hover {
+    background: ${colors.light};
+    box-shadow: -6px 5px ${colors.dark};
+    color: ${colors.dark};
+  }
+`
+
+function Project({
+  url,
+  title,
+  description,
+  images = [],
+  tools,
+  buttonSite,
+  urlSite
+}) {
+  const [hovered, setIsHovered] = useState(false)
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const textDefil = `${title}-`.repeat(4)
+
+  // Slider projects
+  useEffect(() => {
+    const nextSlide = () => {
+      if (images && images?.length > 0) {
+        setCurrentSlide(
+          currentSlide < images?.length - 1 ? currentSlide + 1 : 0
+        )
+      }
+    }
+
+    let timer
+    if (hovered) {
+      timer = setTimeout(nextSlide, 400)
+    }
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [hovered, currentSlide, images])
+
+  return (
+    <Show
+      onMouseEnter={() => {
+        if (!hovered) {
+          setIsHovered(true)
+        } else {
+          setIsHovered(false)
+        }
+      }}
+      onMouseLeave={() => setIsHovered(false)}>
+      <Content
+        style={{
+          display: `${!hovered ? "flex" : "none"}`
+        }}>
+        <TextContainer className="text-container">
+          <ProjectTitle>{title}</ProjectTitle>
+          <p>{description}</p>
+        </TextContainer>
+        <Tools>
+          {tools && tools.map((tool, index) => <span key={index}>{tool}</span>)}
+        </Tools>
+      </Content>
+
+      <ContentHovered
+        style={{
+          display: `${hovered ? "flex" : "none"}`
+        }}>
+        <Image src={images[currentSlide]} alt={`Slide-${currentSlide}`} />
+        <TextHovered aria-hidden="true">{textDefil}</TextHovered>
+        <TextHovered aria-hidden="true">{textDefil}</TextHovered>
+        <ButtonContainer>
+          <Button href={url}>See the code</Button>
+          {buttonSite && <Button href={urlSite}>Visit the site</Button>}
+        </ButtonContainer>
+      </ContentHovered>
+    </Show>
+  )
+}
+
+export default Project
+
+Project.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  images: PropTypes.array,
+  tools: PropTypes.array,
+  buttonSite: PropTypes.bool,
+  urlSite: PropTypes.string
+}
--- src/components/sections/Showcase.jsx
+++ src/components/sections/Showcase.jsx
@@ -1,3 +1,109 @@
-export default function Showcase() {
-  return <div>Showcase</div>
+/* eslint-disable react/prop-types */
+import styled from "styled-components"
+
+import colors from "../../style/colors"
+import queries from "../../style/queries"
+
+import Project from "../Project"
+import booki1 from "../../assets/images/booki/booki_capture01.webp"
+import booki2 from "../../assets/images/booki/booki_capture02.webp"
+import booki3 from "../../assets/images/booki/booki_capture03.webp"
+import events1 from "../../assets/images/77events/77events_capture01.webp"
+import events2 from "../../assets/images/77events/77events_capture02.webp"
+import events3 from "../../assets/images/77events/77events_capture03.webp"
+import carducci1 from "../../assets/images/nina_carducci/NCarducci_capture01.webp"
+import carducci2 from "../../assets/images/nina_carducci/NCarducci_capture02.webp"
+import carducci3 from "../../assets/images/nina_carducci/NCarducci_capture03.webp"
+import OMF1 from "../../assets/images/ohmyfood/ohmyfood_capture01.webp"
+import OMF2 from "../../assets/images/ohmyfood/ohmyfood_capture02.webp"
+import OMF3 from "../../assets/images/ohmyfood/ohmyfood_capture03.webp"
+import bluel1 from "../../assets/images/sophie_bluel/SBluel_capture01.webp"
+import bluel2 from "../../assets/images/sophie_bluel/SBluel_capture02.webp"
+import bluel3 from "../../assets/images/sophie_bluel/SBluel_capture03.webp"
+import kasa1 from "../../assets/images/kasa/kasa_capture01.webp"
+import kasa2 from "../../assets/images/kasa/kasa_capture02.webp"
+import kasa3 from "../../assets/images/kasa/kasa_capture03.webp"
+import argentBank1 from "../../assets/images/argentBank/argentBank_capture01.webp"
+import argentBank2 from "../../assets/images/argentBank/argentBank_capture02.webp"
+import argentBank3 from "../../assets/images/argentBank/argentBank_capture03.webp"
+
+const ShowcaseSection = styled.section`
+  background: ${colors.primary};
+  position: relative;
+  padding: 8vw;
+
+  @media (max-width: ${queries.small}px) {
+    padding: 4vw;
+  }
+`
+
+const ShowcaseTitle = styled.h3`
+  color: ${colors.light};
+  text-align: right;
+  margin: 1rem 0.8rem;
+`
+
+function Showcase() {
+  return (
+    <ShowcaseSection id="showcase">
+      <ShowcaseTitle>Showcase</ShowcaseTitle>
+      <Project
+        url={"https://github.com/thanali/OCP3"}
+        images={[booki1, booki2, booki3]}
+        title={"Booki"}
+        description={"Travel agency homepage"}
+        tools={["html", "css"]}
+        buttonSite
+        urlSite={"https://thanali.github.io/OCP3/"}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP4"}
+        images={[OMF1, OMF2, OMF3]}
+        title={"OhMyFood"}
+        description={`Mobile first app with CSS animations`}
+        tools={["html", "sass"]}
+        buttonSite
+        urlSite={"https://thanali.github.io/OCP4/"}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP6"}
+        images={[bluel1, bluel2, bluel3]}
+        title={"Sophie Bluel"}
+        description={`Dynamic web page with JS`}
+        tools={["sass", "javascript"]}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP8"}
+        images={[kasa1, kasa2, kasa3]}
+        title={"Kasa"}
+        description={"Real estate rental app with React"}
+        tools={["sass", "react"]}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP9"}
+        images={[carducci1, carducci2, carducci3]}
+        title={"Nina Carducci"}
+        description={"SEO optimization"}
+        tools={["css", "javascript"]}
+        buttonSite
+        urlSite={"https://thanali.github.io/OCP9/"}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP10"}
+        images={[events1, events2, events3]}
+        title={"77events"}
+        description={"Debugging"}
+        tools={["sass", "react"]}
+      />
+      <Project
+        url={"https://github.com/thanali/OCP11"}
+        images={[argentBank1, argentBank2, argentBank3]}
+        title={"ArgentBank"}
+        description={"Banking app with React and Redux"}
+        tools={["sass", "react", "redux", "swagger"]}
+      />
+    </ShowcaseSection>
+  )
 }
+
+export default Showcase
