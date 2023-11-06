2a801ce67e91c694ccd8cde912dc7f094714b927
lithana <lbn.nathalie@gmail.com>
2023-11-06 15:23:05 +0100

    Update Slider

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -188,18 +188,25 @@ function Project({
 
   // Slider projects
   useEffect(() => {
+    let timer
+
     const nextSlide = () => {
-      if (images && images?.length > 0 && hovered) {
+      if (images && images?.length > 0) {
         setCurrentSlide(
           currentSlide < images?.length - 1 ? currentSlide + 1 : 0
         )
       }
+      if (hovered) {
+        timer = setTimeout(nextSlide, 400)
+      }
     }
 
-    let timer
     if (hovered) {
       timer = setTimeout(nextSlide, 400)
+    } else {
+      clearTimeout(timer)
     }
+
     return () => {
       clearTimeout(timer)
     }
